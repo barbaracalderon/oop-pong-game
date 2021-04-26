b425e65e71b71d62b7cffae1f914950ffbf6ce1c
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-04-26 11:40:40 -0300

    Created the Ball class, it inherits from Turtle class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -16,9 +17,11 @@ screen.onkey(fun=r_paddle.down, key="Down")
 screen.onkey(fun=l_paddle.up, key="w")
 screen.onkey(fun=l_paddle.down, key="s")
 
+ball = Ball()
 
 game_on = True
 while game_on:
     screen.update()
+    ball.move()
 
 screen.exitonclick()
