173e96a43814fd913ff6dbf936d9dd1022f983b4
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-04-27 09:54:29 -0300

    Added the bouncing aspect when ball hits top and bottom wall

--- ball.py
+++ ball.py
@@ -7,8 +7,13 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.x_move = 0.2
+        self.y_move = 0.2
 
     def move(self):
-        new_x = self.xcor() + 0.1
-        new_y = self.ycor() + 0.1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
--- main.py
+++ main.py
@@ -23,5 +23,7 @@ game_on = True
 while game_on:
     screen.update()
     ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 screen.exitonclick()
